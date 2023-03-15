e35ec2c4057ee4db1aec17603c9d44c3cf9ba9d9
Saidasaradharam <saidasaradharam9@gmail.com>
2023-03-16 00:10:37 +0530

    SMS Authentication

--- app/src/main/java/com/clgproject/myapplicationkoitlin/MainActivity.kt
+++ app/src/main/java/com/clgproject/myapplicationkoitlin/MainActivity.kt
@@ -9,8 +9,10 @@ import android.provider.MediaStore
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.FirebaseApp
 import com.google.firebase.FirebaseException
 import com.google.firebase.auth.FirebaseAuth
@@ -23,21 +25,21 @@ import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var editTextPhoneNumber: Text
+    private lateinit var editTextPhoneNumber: EditText
     private lateinit var buttonGenerateOTP: Button
-    private lateinit var editTextOTP: Text
-    private lateinit var buttonVerifyOTP: Button
+
+    private lateinit var verificationId: String
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         FirebaseApp.initializeApp(this)
-        val auth = FirebaseAuth.getInstance()
+        auth = FirebaseAuth.getInstance()
 
 
         editTextPhoneNumber = findViewById(R.id.editTextPhoneNumber)
         buttonGenerateOTP = findViewById(R.id.buttonGenerateOTP)
-        editTextOTP = findViewById(R.id.editTextOTP)
-        buttonVerifyOTP = findViewById(R.id.buttonVerifyOTP)
 
 
         buttonGenerateOTP.setOnClickListener {
@@ -61,6 +63,7 @@ class MainActivity : AppCompatActivity() {
                     // The SMS verification code has been sent to the provided phone number, we
                     // now need to ask the user to enter the code and then construct a credential
                     // by combining the code with a verification ID.
+                    this@MainActivity.verificationId = verificationId // Save verificationId as a member variable
                     val intent = Intent(this@MainActivity, VerifyActivity::class.java)
                     intent.putExtra("verificationId", verificationId)
                     startActivity(intent)
@@ -76,15 +79,6 @@ class MainActivity : AppCompatActivity() {
             )
         }
 
-        buttonVerifyOTP.setOnClickListener {
-            val otp = editTextOTP.text.toString().trim()
-            if (otp.isEmpty()) {
-                Toast.makeText(this@MainActivity, "Please enter OTP", Toast.LENGTH_SHORT).show()
-            } else {
-                val credential = PhoneAuthProvider.getCredential(verificationId, otp)
-                signInWithPhoneAuthCredential(credential)
-            }
-        }
 
 
     }
@@ -95,7 +89,9 @@ class MainActivity : AppCompatActivity() {
                 if (task.isSuccessful) {
                     // Sign in success, update UI with the signed-in user's information
                     val user = task.result?.user
-                    Toast.makeText(this@MainActivity, "Authentication successful", Toast.LENGTH_SHORT).show()
+                    if (user != null) {
+                        Toast.makeText(this@MainActivity, "Authentication successful", Toast.LENGTH_SHORT).show()
+                    }
                 } else {
                     // Sign in failed, display a message and update the UI
                     Toast.makeText(this@MainActivity, "Authentication failed", Toast.LENGTH_SHORT).show()
--- app/src/main/java/com/clgproject/myapplicationkoitlin/VerifyActivity.kt
+++ app/src/main/java/com/clgproject/myapplicationkoitlin/VerifyActivity.kt
@@ -3,24 +3,42 @@ package com.clgproject.myapplicationkoitlin
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.PhoneAuthProvider
 
 class VerifyActivity : AppCompatActivity() {
 
+    private lateinit var editTextOTP: EditText
+    private lateinit var buttonVerifyOTP: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_verify)
 
+        editTextOTP = findViewById(R.id.editTextOTP)
+        buttonVerifyOTP = findViewById(R.id.buttonVerifyOTP)
         // Get the verification ID from the intent
-        val verificationId = intent.getStringExtra("verificationId")
+        var verificationId = intent.getStringExtra("verificationId")
 
-        // Verify the user's phone number with the verification code
-        if (verificationId != null) {
-            verifyPhoneNumber(verificationId, "123456")
+        buttonVerifyOTP.setOnClickListener()
+        {
+            val otp = editTextOTP.text.toString().trim()
+            if (otp.isEmpty()) {
+                Toast.makeText(this@VerifyActivity, "Please enter OTP", Toast.LENGTH_SHORT).show()
+            } else {
+                val credential = PhoneAuthProvider.getCredential(verificationId.toString(), otp)
+//                signInWithPhoneAuthCredential(credential)
+                if (verificationId != null) {
+                    verifyPhoneNumber(verificationId, otp)
+                }
+
+            }
         }
     }
+
+
 
     private fun verifyPhoneNumber(verificationId: String, code: String) {
         val credential = PhoneAuthProvider.getCredential(verificationId, code)
@@ -31,12 +49,14 @@ class VerifyActivity : AppCompatActivity() {
                 if (task.isSuccessful) {
                     // The user is signed in successfully
                     // Move to the next activity or perform any other required action
+                    Toast.makeText(this@VerifyActivity, "OTP Verified", Toast.LENGTH_LONG).show()
                     val intent = Intent(this, UploadImage::class.java)
                     startActivity(intent)
                     finish()
                 } else {
                     // Sign in failed, display an error message to the user
-                    Toast.makeText(this, "Authentication failed", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this, "Incorrect OTP, Try again.", Toast.LENGTH_LONG).show()
+                    finish()
                 }
             }
     }
