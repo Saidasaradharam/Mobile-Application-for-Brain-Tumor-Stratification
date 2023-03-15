a49592d63bd7c097f10a7698c3e02a1cbccc82e2
Saidasaradharam <saidasaradharam9@gmail.com>
2023-03-16 02:03:51 +0530

    Information Page

---
+++ app/src/main/java/com/clgproject/myapplicationkoitlin/CancerPage.kt
@@ -0,0 +1,19 @@
+package com.clgproject.myapplicationkoitlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class CancerPage : AppCompatActivity() {
+
+    private lateinit var infoTextView: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cancer_page)
+
+        infoTextView = findViewById(R.id.cancer_info)
+        infoTextView.text = "A cancerous brain tumor, also known as a malignant brain tumor, is a fast-growing and aggressive abnormal growth of cells in the brain. Cancerous brain tumors can spread to other parts of the brain and the body, making them much more dangerous than non-cancerous tumors. Symptoms of cancerous brain tumors can include headaches, seizures, personality changes, weakness, and vision or hearing problems. Treatment options for cancerous brain tumors include surgery, radiation therapy, chemotherapy, targeted therapy, and immunotherapy, depending on the tumor's location, size, and type. The prognosis for cancerous brain tumors can vary depending on many factors, including the type and stage of the tumor, the person's age and overall health, and the treatment received. Gliomas are tumors that arise from the glial cells of the brain and can be highly malignant and aggressive. Meningiomas, on the other hand, are tumors that arise from the meninges, the layers of tissue that cover the brain and spinal cord. "
+
+    }
+}
--- app/src/main/java/com/clgproject/myapplicationkoitlin/LearnActivity.kt
+++ app/src/main/java/com/clgproject/myapplicationkoitlin/LearnActivity.kt
@@ -1,11 +1,36 @@
 package com.clgproject.myapplicationkoitlin
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class LearnActivity : AppCompatActivity() {
+
+    private lateinit var cancerButton: Button
+    private lateinit var nonCancerButton: Button
+    private lateinit var infoTextView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_learn)
+
+        infoTextView = findViewById(R.id.tv_info)
+        infoTextView.text = "Brain tumors are abnormal growths of cells in the brain. There are many types of brain tumors, and they can be either cancerous (malignant) or non-cancerous (benign). Some common symptoms of brain tumors include headaches, seizures, and changes in vision or hearing. Treatment options for brain tumors include surgery, radiation therapy, and chemotherapy."
+        cancerButton = findViewById(R.id.cancer_button)
+        nonCancerButton = findViewById(R.id.non_cancer_button)
+
+        cancerButton.setOnClickListener {
+            val intent = Intent(this, CancerPage::class.java)
+            startActivity(intent)
+        }
+
+        nonCancerButton.setOnClickListener {
+            val intent = Intent(this, NonCancerPage::class.java)
+            startActivity(intent)
+        }
     }
 }
